f1b74574e396a800f4c1d2017d02f447f98fd73e
Keishma <kieran.l@yandex.com>
2019-03-04 03:09:20 +0000

    price updates working

--- app/controllers/prices_controller.rb
+++ app/controllers/prices_controller.rb
@@ -6,23 +6,42 @@ class PricesController < ApplicationController
 
   def create
   	@price = Price.new(price_params)
-  	
+      @shop = params[:price][:shop_id]
+      @product = params[:price][:product_id]
+
   	if @price.save
-  		@shop = params[:price][:shop_id]
-  		
-  		@product = params[:price][:product_id]
-
-  		@old = Price.where(product_id: 1).where(shop_id: 1).last(2)[0]
-  		@old.current = true
-  		@old.save
-
-  		flash[:success] = "Price saved - Shop:#{@shop} Product:#{@product}/ old price = #{@old.price}"
+      set_last
+  		flash[:success] = "Price saved - id: #{@price.id} ||"
   		redirect_to @price
-
   	else
   		redirect_to new_price_path, notice: 'failed'
   	end
 
+  end
+
+  def set_last
+    @p_id = @price.id
+    return if @p_id.nil?
+    @prices = Price.all
+    that = Price.where(product_id: @price.product_id, shop_id: @price.shop_id).last(2)[0]
+    prev_id = that.id
+    old = Price.find(prev_id)
+    price_off(old)
+  end
+
+  def putsy(block)
+    puts " #{block}"
+    puts "-------------------------"
+  end
+
+    def putsx(block)
+    puts " xxx#{block}"
+    puts "-------------------------"
+  end
+
+  def price_off(block)
+      block.current = false
+      block.save
   end
 
   def show
